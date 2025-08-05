e1cf7bd59d6c45a61e04385704d7a4bc90ce9022
Alessio Galatolo <galatoloalessio@gmail.com>
2025-08-05 16:41:37 +0200

    Increase app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "agdesigns.elevatefitness"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.0.1"
+        versionCode = 2
+        versionName = "0.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
